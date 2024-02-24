ca863c0a817e2d1dbf0690a546d69299e37cc87b
Samari Oglesby <SamariOglesby16@gmail.com>
2024-02-23 20:41:19 -0500

    Login Two Commit

---
+++ LoginTwo/app/src/main/python/example.py
@@ -0,0 +1,62 @@
+# import main Flask class and request object
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
+
+
+# allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return 'POST'
+    # otherwise handle the GET request
+    return ' GET'
+
+
+
+# GET requests will be blocked
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+    language = request_data['language']
+    framework = request_data['framework']
+
+    # two keys are needed because of the nested object
+    python_version = request_data['version_info']['python']
+
+    # an index is needed because of the array
+    example = request_data['examples'][0]
+
+    boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework,
+                       python_version, example, boolean_test)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
---
+++ LoginTwo/app/src/main/python/login.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import json
+
+app = Flask("Login")
+@app.route('/login', methods=['POST'])
+def login():
+    request_data = request.get_json()
+    username = request_data['username']
+    password = request_data['password']
+
+    print(f'Username : {username}, Password: {password}')
+    success = (username == 'Sammie') and (password == "shoes")
+
+    return json.dumps({'login': success})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', 5000, True)
